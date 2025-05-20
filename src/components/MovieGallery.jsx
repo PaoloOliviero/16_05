474a20e37ea781d5f9225dee6479103d3f9d5ac0
PaoloOliviero <paolo.oliviero3@gmail.com>
2025-05-20 10:58:42 +0200

    Commit 7

--- src/components/MovieGallery.jsx
+++ src/components/MovieGallery.jsx
@@ -1,91 +1,87 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
 const API_KEY = "996e7de7";
 
-const lordOfTheRingsSaga = [
-  "The Lord of the Rings: The Fellowship of the Ring",
-  "The Lord of the Rings: The Two Towers",
-  "The Lord of the Rings: The Return of the King",
-  "The Hobbit: An Unexpected Journey",
-  "The Hobbit: The Desolation of Smaug",
-  "The Hobbit: The Battle of the Five Armies",
-];
+const sagas = {
+  lordOfTheRings: [
+    "The Lord of the Rings: The Fellowship of the Ring",
+    "The Lord of the Rings: The Two Towers",
+    "The Lord of the Rings: The Return of the King",
+    "The Hobbit: An Unexpected Journey",
+    "The Hobbit: The Desolation of Smaug",
+    "The Hobbit: The Battle of the Five Armies",
+  ],
+  missionImpossible: [
+    "Mission: Impossible",
+    "Mission: Impossible 2",
+    "Mission: Impossible III",
+    "Mission: Impossible - Ghost Protocol",
+    "Mission: Impossible - Rogue Nation",
+    "Mission: Impossible - Fallout",
+  ],
+  rocky: ["Rocky", "Rocky II", "Rocky III", "Rocky IV", "Rocky V", "Rocky Balboa"],
+};
 
-const missionImpossibleSaga = [
-  "Mission: Impossible",
-  "Mission: Impossible 2",
-  "Mission: Impossible III",
-  "Mission: Impossible - Ghost Protocol",
-  "Mission: Impossible - Rogue Nation",
-  "Mission: Impossible - Fallout",
-];
+class MovieGallery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: {
+        lordOfTheRings: [],
+        missionImpossible: [],
+        rocky: [],
+      },
+    };
+  }
 
-const rockySaga = ["Rocky", "Rocky II", "Rocky III", "Rocky IV", "Rocky V", "Rocky Balboa"];
+  componentDidMount() {
+    this.fetchAllMovies();
+  }
 
-const MovieGallery = () => {
-  const [movies, setMovies] = useState({ lordOfTheRings: [], missionImpossible: [], rocky: [] });
+  async fetchAllMovies() {
+    const moviesData = {};
+    for (const category in sagas) {
+      moviesData[category] = await this.fetchMovies(sagas[category]);
+    }
+    this.setState({ movies: moviesData });
+  }
 
-  useEffect(() => {
-    const fetchMovies = async (category, movieList) => {
-      const movieData = [];
-
-      for (const title of movieList) {
+  async fetchMovies(movieList) {
+    const movieData = await Promise.all(
+      movieList.map(async (title) => {
         const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&t=${encodeURIComponent(title)}`);
         const data = await response.json();
 
         if (data.Response !== "False") {
-          movieData.push({
-            imdbID: data.imdbID,
-            poster: data.Poster,
-          });
-
-          console.log(`Film trovato (${category}):`, {
-            title: data.Title,
-            year: data.Year,
-            poster: data.Poster,
-          });
+          console.log("Search result:", data);
+          return { imdbID: data.imdbID, poster: data.Poster };
         }
-      }
+        return null;
+      })
+    );
 
-      setMovies((prevMovies) => ({ ...prevMovies, [category]: movieData }));
-    };
+    return movieData.filter((movie) => movie !== null);
+  }
 
-    fetchMovies("lordOfTheRings", lordOfTheRingsSaga);
-    fetchMovies("missionImpossible", missionImpossibleSaga);
-    fetchMovies("rocky", rockySaga);
-  }, []);
-
-  return (
-    <Container>
-      <h2 className="my-4 text-start">The Lord of the Rings + The Hobbit Saga</h2>
-      <Row className="justify-content-center g-3">
-        {movies.lordOfTheRings.map((movie) => (
-          <Col key={movie.imdbID} xs={6} sm={4} md={3} lg={2} className="d-flex justify-content-center">
-            <img src={movie.poster} alt="Movie Poster" className="img-fluid rounded" />
-          </Col>
+  render() {
+    return (
+      <Container>
+        {Object.entries(this.state.movies).map(([category, movies]) => (
+          <React.Fragment key={category}>
+            <h2 className="my-4 pr-5 text-start">{category.split(/(?=[A-Z])/).join(" ")}</h2>
+            <Row className="justify-content-center g-3">
+              {movies.map((movie) => (
+                <Col key={movie.imdbID} xs={6} sm={4} md={3} lg={2} className="d-flex justify-content-center">
+                  <img src={movie.poster} alt="Movie Poster" className="img-fluid rounded" />
+                </Col>
+              ))}
+            </Row>
+          </React.Fragment>
         ))}
-      </Row>
-
-      <h2 className="my-4 text-start">Mission: Impossible Saga</h2>
-      <Row className="justify-content-center g-3">
-        {movies.missionImpossible.map((movie) => (
-          <Col key={movie.imdbID} xs={6} sm={4} md={3} lg={2} className="d-flex justify-content-center">
-            <img src={movie.poster} alt="Movie Poster" className="img-fluid rounded" />
-          </Col>
-        ))}
-      </Row>
-
-      <h2 className="my-4 text-start">Rocky Saga</h2>
-      <Row className="justify-content-center g-3">
-        {movies.rocky.map((movie) => (
-          <Col key={movie.imdbID} xs={6} sm={4} md={3} lg={2} className="d-flex justify-content-center">
-            <img src={movie.poster} alt="Movie Poster" className="img-fluid rounded" />
-          </Col>
-        ))}
-      </Row>
-    </Container>
-  );
-};
+      </Container>
+    );
+  }
+}
 
 export default MovieGallery;
